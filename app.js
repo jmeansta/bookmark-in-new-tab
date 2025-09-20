beb77c60ef49c8be4401b890fd5f0fe05a71b415
joannJonathanMeans <jontheinventor@gmail.com>
2025-09-20 16:45:29 -0700

    Added ability to choose the starting folder for the itterator

--- app.js
+++ app.js
@@ -3,42 +3,57 @@ function err(message) {
   $("errorText").innerHTML = message;
   $("errorText").style = "color: red;"
 }
-function newLi(text,indent) {
-  var elm = document.createElement("li")
-  elm.innerHTML = text
-  elm.style = "text-indent: " + indent*20 + "px;"
-  $("bmList").appendChild(elm)
+
+function newElement(bookmarkItem,indent) {
+  // if (indent<=2) { // temporary decluttering measure
+    if (bookmarkItem.url) {
+      var elm = document.createElement("a"); // bookmarks
+      elm.href = bookmarkItem.url;
+      indent++
+    } else {
+      var elm = document.createElement("p"); // folders
+    }
+    elm.innerHTML = bookmarkItem.title;
+    elm.title = bookmarkItem.id;
+    // elm.innerHTML = indent.toString()+" - "+bookmarkItem.title;
+    // elm.innerHTML = bookmarkItem.title+" ("+bookmarkItem.id+")";
+    elm.style = "margin: 0px; margin-left: " + indent*40 + "px;"
+
+    if (bookmarkItem.url == "data:") {
+      var elm = document.createElement("hr"); // separators
+      elm.align = "right";
+      elm.width = $("bmDiv").clientWidth-indent*40 + "px";
+    }
+
+    $("bmDiv").appendChild(elm)
+  // } // closing the temporary decluttering measure
 }
 
-function newItem(text,indent,path=null) {
-  if (path == null) {
-    var elm = document.createElement("p");
-  } else {
-    var elm = document.createElement("a");
-    elm.href = path;
-  }
-  elm.innerHTML = indent.toString()+" - "+text;
-  elm.style = "text-indent: " + indent*3 + "em;"
-  $("bmDiv").appendChild(elm)
-}
-
-function printItems(bookmarkItem, indent) {
-  if (bookmarkItem.url) {
-    newItem(bookmarkItem.title,indent,bookmarkItem.url);
-  } else {
-    newItem(bookmarkItem.title,indent);
+function traverseTree(bookmarkItem, indent, printingChildren) {
+  if (!bookmarkItem.url && bookmarkItem.title) {
     indent++;
-    if (bookmarkItem.children) {
-      for (const child of bookmarkItem.children) {
-        printItems(child, indent);
+    // if it's a folder, indent the next line
+    // Don't do this for the root element (no title)
+  }
+
+  if (printingChildren) { newElement(bookmarkItem,indent) }
+
+  if (bookmarkItem.children) {
+    for (const child of bookmarkItem.children) {
+      // if (printingChildren || bookmarkItem.id == "toolbar_____") {
+      if (printingChildren || bookmarkItem.id == "root________") {
+      // if (bookmarkItem.id == "root________") {
+        traverseTree(child, indent, true);
+      } else {
+        traverseTree(child, indent, false);
       }
     }
-    indent--;
   }
+  indent--;
 }
 
 function printTree(bookmarkItems) {
-  printItems(bookmarkItems[0], 0);
+  traverseTree(bookmarkItems[0], 0, false);
 }
 
 function onRejected(error) {
