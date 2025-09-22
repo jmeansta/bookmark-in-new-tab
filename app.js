71e89d745615209c943f431ef228d8cca26b583d
joannJonathanMeans <jontheinventor@gmail.com>
2025-09-22 10:47:44 -0700

    Cleaned up unused functions from last commit

--- app.js
+++ app.js
@@ -22,29 +22,6 @@ function newElement(bookmarkItem,indent) {
   $("bookmarkBar").appendChild(elm)
 }
 
-function traverseTree(bookmarkItem, indent, printingChildren) {
-  if (printingChildren) {
-    newElement(bookmarkItem,indent)
-  }
-
-  if (bookmarkItem.children) {
-    indent++;
-    for (const child of bookmarkItem.children) {
-      // if (printingChildren || bookmarkItem.id == "toolbar_____") {
-      if (printingChildren || bookmarkItem.id == "root________") {
-        traverseTree(child, indent, true);
-      } else {
-        traverseTree(child, indent, false);
-      }
-    }
-    indent--;
-  }
-}
-
-function startTraversal(bookmarkItems) {
-  traverseTree(bookmarkItems[0], 0, false);
-}
-
 function onRejected(error) {
   var elm = document.createElement("p");
   elm.innerHTML = `An error occured: ${error}`
@@ -58,37 +35,15 @@ function newFolder(id) {
   $("bookmarkFolders").appendChild(folder)
 }
 
-async function restoreOptions() {
-  const options = await browser.storage.local.get(["selectedFolders"]);
-  // console.log(options)
-  // options.selectedFolders.forEach((id) => newFolder(id));
-  // let treePromise = browser.bookmarks.getTree();
-  // treePromise.then(startTraversal, onRejected);
-  // have the tree traversal function get the ids of the folders to load
-  // that function should traverse the tree, looking for folders with those ids
-  // and then make folders in the folder div with those names
-};
-
 async function startTraversalToRestoreFolders(bookmarkItems) {
   traverseTreeToRestoreFolders(bookmarkItems[0]);
-}
-
-function checkAndAddFolder(id,bookmarkItem) {
-  if (bookmarkItem.id == id) {
-    newFolder(bookmarkItem.title)
-  }
 }
 
 async function traverseTreeToRestoreFolders(bookmarkItem) {
   const options = await browser.storage.local.get(["selectedFolders"]);
 
-  options.selectedFolders.forEach((id) => checkAndAddFolder(id,bookmarkItem));
-  // if (bookmarkItem.id == id) {newFolder(bookmarkItem)}
-
-  // options.selectedFolders.forEach((id) => function () {if (bookmarkItem.id == id) {alert(bookmarkItem.title)}});
-  // if (printingChildren) {
-  //   newElement(bookmarkItem,indent)
-  // }
+  options.selectedFolders.forEach(function(id) {if (bookmarkItem.id == id) {newFolder(bookmarkItem.title)}});
+  
   if (bookmarkItem.children) {
     for (const child of bookmarkItem.children) {
       traverseTreeToRestoreFolders(child);
@@ -96,6 +51,5 @@ async function traverseTreeToRestoreFolders(bookmarkItem) {
   }
 }
 
-// window.addEventListener( 'load', startTraversalToRestoreFolders );
 let treePromise = browser.bookmarks.getTree();
 treePromise.then(startTraversalToRestoreFolders, onRejected);
