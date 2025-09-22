34d79c67308cb6fb8d66e1a4adfbc62f7dc8e086
joannJonathanMeans <jontheinventor@gmail.com>
2025-09-21 23:20:58 -0700

    bookmarks-to-be-displayed-in-new-tab are now loaded from local storage when opening a new tab

--- app.js
+++ app.js
@@ -1,64 +1,19 @@
 function $(id) {return document.getElementById(id)}
-function err(message) {
-  $("errorText").innerHTML = message;
-  $("errorText").style = "color: red;"
+
+function newElement(id) {
+  var folder = document.createElement("p")
+  folder.innerHTML = id;
+  $("bookmarkBar").appendChild(folder)
 }
 
-function newElement(bookmarkItem,indent) {
-  // if (indent<=2) { // temporary decluttering measure
-    if (bookmarkItem.url) {
-      var elm = document.createElement("a"); // bookmarks
-      elm.href = bookmarkItem.url;
-      indent++
-    } else {
-      var elm = document.createElement("p"); // folders
-    }
-    elm.innerHTML = bookmarkItem.title;
-    elm.title = bookmarkItem.id;
-    // elm.innerHTML = indent.toString()+" - "+bookmarkItem.title;
-    // elm.innerHTML = bookmarkItem.title+" ("+bookmarkItem.id+")";
-    elm.style = "margin: 0px; margin-left: " + indent*40 + "px;"
-
-    if (bookmarkItem.url == "data:") {
-      var elm = document.createElement("hr"); // separators
-      elm.align = "right";
-      elm.width = $("bmDiv").clientWidth-indent*40 + "px";
-    }
-
-    $("bmDiv").appendChild(elm)
-  // } // closing the temporary decluttering measure
+function l(message) {
+  console.log(message)
 }
 
-function traverseTree(bookmarkItem, indent, printingChildren) {
-  if (!bookmarkItem.url && bookmarkItem.title) {
-    indent++;
-    // if it's a folder, indent the next line
-    // Don't do this for the root element (no title)
-  }
+async function restoreOptions() {
+  const options = await browser.storage.local.get(["selectedFolders"]);
+  console.log(options)
+  options.selectedFolders.forEach((id) => newElement(id));
+};
 
-  if (printingChildren) { newElement(bookmarkItem,indent) }
-
-  if (bookmarkItem.children) {
-    for (const child of bookmarkItem.children) {
-      // if (printingChildren || bookmarkItem.id == "toolbar_____") {
-      if (printingChildren || bookmarkItem.id == "root________") {
-      // if (bookmarkItem.id == "root________") {
-        traverseTree(child, indent, true);
-      } else {
-        traverseTree(child, indent, false);
-      }
-    }
-  }
-  indent--;
-}
-
-function printTree(bookmarkItems) {
-  traverseTree(bookmarkItems[0], 0, false);
-}
-
-function onRejected(error) {
-  newFold(`An error: ${error}`,0);
-}
-
-let treePromise = browser.bookmarks.getTree();
-treePromise.then(printTree, onRejected);
+window.addEventListener( 'load', restoreOptions );
