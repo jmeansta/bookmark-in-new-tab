2a7c6334c94b17222bcdcb4e2216923518f0ce55
joannJonathanMeans <jontheinventor@gmail.com>
2025-09-24 01:19:40 -0700

    Implemented desired style for bookmark folders in caroselTest.html

--- app.js
+++ app.js
@@ -1,5 +1,27 @@
 function $(id) {return document.getElementById(id)}
 
+function watchOverflow(el) {
+  function update() {
+    const overflowing = el.scrollWidth > el.clientWidth;// || el.scrollHeight > el.clientHeight;
+    el.classList.toggle("is-overflowing", overflowing);
+  }
+
+  // Run once initially
+  update();
+
+  // Watch for size changes
+  const resizeObserver = new ResizeObserver(update);
+  resizeObserver.observe(el);
+}
+
+function displayFolderBookmarks(bmFolderElm) {
+  var bmid = bmFolderElm.dataset.bookmarkItem
+  while ($("bookmarkBar").firstChild) {
+      $("bookmarkBar").removeChild($("bookmarkBar").firstChild);
+  }
+  treePromise.then((tree) => startTraversal_folderContents (tree,bmid), onRejected)
+}
+
 function newBookmark(bookmarkItem,indent=0) {
   var elm = document.createElement("p");
   elm.innerHTML = "bookmarkItem with no URL passed to newBookmark"
@@ -16,19 +38,10 @@ function newBookmark(bookmarkItem,indent=0) {
     elm.style = "margin: 0px; margin-left: " + indent*40 + "px;"
   }
   $("bookmarkBar").appendChild(elm)
-  
-}
-
-function displayFolderBookmarks(bmFolderElm) {
-  var bmid = bmFolderElm.dataset.bookmarkItem
-  while ($("bookmarkBar").firstChild) {
-      $("bookmarkBar").removeChild($("bookmarkBar").firstChild);
-  }
-  treePromise.then((tree) => startTraversal_folderContents (tree,bmid), onRejected)
 }
 
 function newFolder(bookmarkItem) {
-  var folder = document.createElement("p")
+  var folder = document.createElement("div")
   folder.innerHTML = bookmarkItem.title;
   folder.dataset.bookmarkItem = bookmarkItem.id
   // folder.id = `bmid${bookmarkItem.id}`
@@ -62,8 +75,8 @@ async function traverseTree_folderContents(bookmarkItem,bmid,isChild) {
 
   if (bookmarkItem.children) {
     for (const child of bookmarkItem.children) {
-      // if (isChild || bookmarkItem.id == bmid) {
-      if (bookmarkItem.id == bmid) {
+      // if (isChild || bookmarkItem.id == bmid) { //show children and nested children
+      if (bookmarkItem.id == bmid) { // only show the direct children
         traverseTree_folderContents(child, bmid, true);
       } else {
         traverseTree_folderContents(child, bmid, false);
@@ -77,11 +90,13 @@ async function startTraversal_savedFolders(bookmarkItems) {
 }
 
 function startTraversal_folderContents(bookmarkItems,bmid) {
-  traverseTree_folderContents(bookmarkItems[0],bmid)
+  traverseTree_folderContents(bookmarkItems[0],bmid,false)
 }
 
 let treePromise = browser.bookmarks.getTree();
 treePromise.then(startTraversal_savedFolders, onRejected);
+document.querySelectorAll(".dividerBox").forEach(watchOverflow);
+
 
 // displayFolderBookmarks($("bookmarkFolders").firstChild.dataset.bookmarkItem)
 
